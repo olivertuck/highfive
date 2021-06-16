8ef04e29aa00b6181a8932fa46d9fa6f3efec672
Oli <oliver.d.tuck@gmail.com>
2021-06-16 18:29:25 +0100

    Init

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import 'styles/globals.css';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Home | Highfive</title>
+      <meta name="description" content="Home | Highfive" />
+    </Head>
+    <div>Highfive</div>
+  </>
+);
+
+export default Home;
